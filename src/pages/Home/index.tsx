e647237648dcb33c72ad1def65622592cad02bfd
Marthin Korb <marthinkorb@gmail.com>
2020-10-28 00:20:10 -0300

    Building home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,35 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../Hooks/auth';
+import { FiPower } from 'react-icons/fi';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
+import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
-    const { signOut } = useContext(AuthContext);
+    const { signOut, user } = useContext(AuthContext);
 
     return (
-        <>
-            <div
-                style={{
-                    color: '#32C7AB',
-                    margin: 30,
-                }}
-            >
-                <h1>Bem vindo</h1>
-            </div>
-            <button type="button" onClick={signOut}>
-                Sair
-            </button>
-        </>
+        <Container>
+            <Header>
+                <HeaderContent>
+                    <img src="" alt="" />
+
+                    <Profile>
+                        {/* <img src={user.avatar_url} alt="" /> */}
+                        <div>
+                            <span>Bem vindo, </span>
+                            <Link to="/profile">
+                                <strong> {user.name} </strong>
+                            </Link>
+                        </div>
+                    </Profile>
+
+                    <button type="button" onClick={signOut}>
+                        <FiPower />
+                    </button>
+                </HeaderContent>
+            </Header>
+        </Container>
     );
 };
 
